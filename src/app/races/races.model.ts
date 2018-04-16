5905b97152c8cae1082ef2b3d909d941d0936f9b
Daniel Hernandez <weissguts@gmail.com>
2018-04-16 16:00:14 -0600

    Progress towards api

--- src/app/races/races.model.ts
+++ src/app/races/races.model.ts
@@ -1,9 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+activeApiURL = '../assets/races.json';
+
+@Injectable()
 export class Race {
   public date: string;
   public raceName: string;
   public distance: string;
   public location: string;
-  constructor(date: string, raceName: string, distance: string, location: string) {
+  constructor(private http: HttpClient, date: string, raceName: string, distance: string, location: string) {
     this.date = date;
     this.raceName = raceName;
     this.distance = distance;
