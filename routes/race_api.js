8c9a9c0ddc40762fad351c2d6036f6d10adf35b3
Daniel Hernandez <weissguts@gmail.com>
2018-04-23 17:58:30 -0600

    Some progress. /api/races still returning empty array.

--- routes/race_api.js
+++ routes/race_api.js
@@ -1,16 +1,52 @@
-var raceArray = require("../models/race");
+// var raceArray = require("../models/race");
+var request = require("request");
+
+
+// request.get(options).pipe(request.put('http://localhost:8080/api/races'));
+module.exports = function (app) {
+    var options = { method: 'GET',
+        url: 'http://api.amp.active.com/v2/search/',
+        qs:
+            { radius: '50',
+                city: 'denver',
+                current_page: '1',
+                per_page: '10',
+                sort: 'distance',
+                topic: 'running',
+                start_date: '2018-04-17..2018-12-31',
+                exclude_children: 'true',
+                api_key: 't773nv58p6ysdh4bvn5yavvp' },
+        headers:
+            { 'Postman-Token': '929d5e5d-48f5-4ae7-a15d-6933d07229ca',
+                'Cache-Control': 'no-cache' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
+
+
+    app.get("/api/races", function(req, res) {
+        res.json(req.body);
+    });
+
+    app.post("/api/races", function (req, res) {
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+            res.send(body);
+        });
+
+    })
+};
+
+
+
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 
-module.exports = function (app) {
-    app.get("/api/races", function(req, res) {
-        res.json(req.body);
-    });
-
-    app.post("/api/races", function (req, res) {
-        res.send(raceArray);
-    })
-};
--- server.js
+++ server.js
@@ -2,16 +2,15 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var app = express();
 var path = require("path");
 // Sets up the Express App
 
 // =============================================================
-var app = express();
 var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({
-    extended: false }));
-app.use(bodyParser.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 // ================================================================================
 // ROUTER
