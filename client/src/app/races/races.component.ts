861a69f8142d70ba468c0dd6c1d87e48909ee029
Daniel Hernandez <weissguts@gmail.com>
2018-04-24 13:27:47 -0600

    Progress; currently can concole log items in angular,and are passing through constructor

--- client/src/app/races/races.component.ts
+++ client/src/app/races/races.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Race, RacesService} from './races.service';
 import { HttpClient } from '@angular/common/http';
+import {RacesModel} from './races.model';
 
 @Component({
   selector: 'dh-races',
@@ -9,10 +10,32 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./races.component.scss']
 })
 export class RacesComponent {
+  public id: string;
+  public date: string;
+  public raceName: string;
+  public website: string;
   error: any;
   headers: string[];
   race: Race;
+  racesM: RacesModel[] = [
+    new RacesModel(this.id, this.date, this.raceName, this.website)
+  ];
   constructor(private racesService: RacesService) {
+    var raceArray = [];
+    this.racesService.getRaceResponse()
+      .subscribe(resp => {
+        raceArray.push(resp.body);
+        for (let obj of raceArray) {
+          for (let i in obj) {
+            console.log(
+            this.id = obj[i].id,
+            this.date = obj[i].activityStartDate,
+            this.raceName = obj[i].assetName,
+            this.website = obj[i].website
+          )
+          }
+        }
+      });
   }
 
   showRace() {
@@ -22,6 +45,7 @@ export class RacesComponent {
     );
     console.log(this.race + ' Test');
   }
+  // Gets data from API and console logs it to test.
   showRaceReponse() {
     var raceArray = [];
     this.racesService.getRaceResponse()
